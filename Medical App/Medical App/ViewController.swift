cd7f456fb060c211021dc0cdab4aa2ba88fc8972
Prajot10 <102917391+Prajot10@users.noreply.github.com>
2025-01-23 15:53:52 +0530

    Add files via upload

---
+++ Medical App/Medical App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Medical App
+//
+//  Created by Prajot Awale on 21/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
