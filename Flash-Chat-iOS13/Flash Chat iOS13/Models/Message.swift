363cdfb5e54b7449c200acfef0614be3306f3105
Prajot10 <102917391+Prajot10@users.noreply.github.com>
2025-01-23 15:37:55 +0530

    Add files via upload

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+// Message
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Prajot Awale on 10/04/24.
+//  Copyright © 2024 Prajot Awale. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
