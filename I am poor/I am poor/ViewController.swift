2c53b4ca2212ebc13058e53ba641c51bf1d7a6ff
Prajot10 <102917391+Prajot10@users.noreply.github.com>
2025-01-23 15:46:03 +0530

    Add files via upload

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Prajot Awale on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
