75a949b659d3450faff4803c48d570c767d045d6
Prajot10 <102917391+Prajot10@users.noreply.github.com>
2025-01-23 15:28:38 +0530

    Add files via upload

---
+++ BMI-Calculator LayoutPractice/BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        
+        heightLabel.text = String(format:"%.2f",sender.value)
+    }
+    
+    
+    @IBAction func weightSliderChnaged(_ sender: UISlider) {
+        
+        weightLabel.text = String(format:"%.0f",sender.value)
+    }
+    
+}
+
