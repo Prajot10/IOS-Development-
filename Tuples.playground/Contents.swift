b3da87a9c1d5acc6c61a1c6038985d652f484144
Prajot10 <102917391+Prajot10@users.noreply.github.com>
2025-01-27 10:54:12 +0530

    Add files via upload

---
+++ Tuples.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let toples1 = ("Angela", 12)
+
+let touples2 = (name: "Angela", age: 12)
+
+print(touples2.name)
+
+
+let touple3: (name: String, age: Int)
+
+touple3 = ("Agni", 12)
+
+print(touple3.name)
+print(touple3.age)
